c9d0a4055e684d1a74f81993ad9eac7434019f8a
Z3bzonic <107925003+Z3bzonic@users.noreply.github.com>
2022-06-27 15:51:45 +0200

    Add files via upload

---
+++ Assignment_Euler_project/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+List <int> cijferReeks = new List<int> ();
+int som=0;
+
+for (int i = 0; i < 10001; i++)
+{
+    if ((i % 3)== 0 || (i % 5) == 0)
+    {
+        cijferReeks.Add(i);       
+    }
+
+}
+
+foreach (var item in cijferReeks)
+{
+    som = som + item;
+    Console.Write($"{item} +");
+    if (som == 234168)
+    {
+        break;
+    }
+}
+Console.WriteLine();
+Console.WriteLine($"{som} is de uitkomst");
