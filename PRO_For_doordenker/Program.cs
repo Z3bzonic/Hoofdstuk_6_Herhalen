f2f693a1a2e0bd9e75697e94c1888570c75647c0
Z3bzonic <107925003+Z3bzonic@users.noreply.github.com>
2022-06-27 21:42:51 +0200

    Add files via upload

---
+++ PRO_For_doordenker/Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Geef de driehoekbreedte in!");
+int invoer = int.Parse(Console.ReadLine());
+string teken = "*";
+
+
+
+Console.Write(teken);
+Console.Write("\n");
+
+for (int i = 0; i < (invoer / 4); i++)
+{
+    Console.Write(teken);
+}
+Console.Write("\n");
+
+for (int i = 0; i < (invoer / 2); i++)
+{
+    Console.Write(teken);
+}
+Console.Write("\n");
+
+for (int i = 0; i < (invoer); i++)
+{
+    Console.Write(teken);
+}
+Console.Write("\n");
+
+for (int i = 0; i < (invoer / 2); i++)
+{
+    Console.Write(teken);
+}
+Console.Write("\n");
+
+for (int i = 0; i < (invoer / 4); i++)
+{
+    Console.Write(teken);
+}
+Console.Write("\n");
+
+    Console.Write(teken);
