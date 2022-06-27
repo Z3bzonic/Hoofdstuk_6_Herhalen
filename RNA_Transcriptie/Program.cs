2cb7004123be11f8fa7d68b03104f338286d0ad7
Z3bzonic <107925003+Z3bzonic@users.noreply.github.com>
2022-06-27 12:19:26 +0200

    Add files via upload

---
+++ RNA_Transcriptie/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+char invoer;
+string rna = "";
+
+do {
+    Console.WriteLine("Geef een nucleotideletter op");
+    char.TryParse(Console.ReadLine(), out invoer);
+    switch (invoer)
+    {
+        case 'g':
+        case 'G':
+            invoer = 'C';
+            break;
+        case 'c':
+        case 'C':
+            invoer = 'G';
+            break;
+        case 't':
+        case 'T':
+            invoer = 'A';
+            break;
+        case 'a':
+        case 'A':
+            invoer = 'U';
+            break;
+        default:
+            break;
+    }
+    rna += invoer;
+    Console.Write($"{rna} + ");
+} while (invoer == 'G' || invoer == 'C' || invoer == 'T' || invoer == 'A' || invoer == 'g' || invoer == 'c' || invoer == 't' || invoer == 'a' || invoer == 'U');
+
+// in hindsight had ik beter geen do while gebruikt omdat ELKE mogelijkheid van de switch case ook moet herhaald worden
+// indien je BV U niet specifieert zal de A onthouden worden en wordt de input overgeslagen
+// lijkt me een bug?? maar kan het niet oplossen door een waarde reset want de invoer
+// MOET dan voorkomen in de while 
