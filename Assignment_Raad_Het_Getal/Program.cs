2b754242511b1346b21b095ed8b8c1a53e309a9b
Z3bzonic <107925003+Z3bzonic@users.noreply.github.com>
2022-06-28 12:55:37 +0200

    Add files via upload

---
+++ Assignment_Raad_Het_Getal/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+int getal;
+int poging = 0;
+int aantalPogingen = 0;
+int onderGrens= 0; //deel2
+int bovenGrens= 100; //deel2
+int maxPogingen= bovenGrens; // deel4
+string pogingString;
+bool gevonden = false;
+Random rand = new Random();
+getal = rand.Next(0, bovenGrens);//deel4
+
+while (!gevonden)
+{
+    aantalPogingen++;
+    Console.WriteLine($"Geef een getal tussen {onderGrens} en {bovenGrens}"); // deel2
+    pogingString = Console.ReadLine();
+    poging = int.Parse(pogingString);
+    if ((poging >= onderGrens) && (poging <= bovenGrens))// deel3&4
+    {
+
+    if (getal > poging)
+    {
+        onderGrens = poging;//deel2
+        Console.WriteLine($"Het gezochte getal is groter, probeer opnieuw.");
+        maxPogingen = (bovenGrens - onderGrens); //deel4
+        Console.WriteLine($"Nog {maxPogingen} over");//deel4
+    }
+    else if (getal < poging)
+    {
+        bovenGrens = poging;//deel2
+        Console.WriteLine($"Het gezochte getal is kleiner, probeer opnieuw.");
+        maxPogingen = (bovenGrens - onderGrens); //deel4
+        Console.WriteLine($"Nog {maxPogingen} over");//deel4
+    }
+    else
+        gevonden = true;
+    }
+}
+Console.WriteLine($"Gevonden! Het te zoeken getal was inderdaad {getal} je had er {aantalPogingen} pogingen voor nodig.");
