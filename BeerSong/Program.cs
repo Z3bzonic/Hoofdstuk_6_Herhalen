7a6b17abd2a20f97c8d0003c3a778188f86312f1
Z3bzonic <107925003+Z3bzonic@users.noreply.github.com>
2022-06-29 07:29:39 +0200

    Add files via upload

---
+++ BeerSong/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+int counter;
+
+while (true)
+{
+    for (counter = 99; counter > 0; counter--)
+    {
+        Console.WriteLine($"{counter} bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more.");
+        Console.ReadLine();
+    }
+    Console.WriteLine($"No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more.");
+}
+
